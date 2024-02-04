ea0ef6212e060595a6c3a826280780f372f05613
Vicky041 <osemekev22@gmail.com>
2024-02-04 02:21:25 +0100

    class lesson

---
+++ math.js
@@ -0,0 +1,25 @@
+console.log('Hello from math.js')
+
+ //exports.//add = (a, b) => {
+//     return a + b
+// }
+
+//exports. //sub = (a, b) => {
+//     return a - b
+// }
+
+// exports.name = 'vicky the engr';
+// exports.METHOD = () => {
+//     console.log('Hello from math.js');
+// };
+
+module.exports = {
+    add: (a, b) => {
+        return a + b
+    },
+    sub: (a, b) => {
+        return a - b
+    }
+}
+
+//module.exports = math;
